62de17289388e165f9cb686552a4cbed73d3701e
Ronald <rogozinronald@gmail.com>
2023-03-30 20:04:52 +0300

    quick fix

--- src/js/PixabayAPI.js
+++ src/js/PixabayAPI.js
@@ -5,7 +5,7 @@ export async function PixabayAPI(query, page = 1) {
   const API_KEY = '34864720-c738fbe20d15b8cdd150dfeaa';
   try {
     const response = await axios.get(
-      `${BASE_URL}?key=${API_KEY}&q=${query}&image_type="photo" &orientation="horizontal" &safesearch="true"&page=${page}&per_page=40`
+      `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo &orientation=horizontal &safesearch=true&page=${page}&per_page=40`
     );
     return response.data;
   } catch (error) {
